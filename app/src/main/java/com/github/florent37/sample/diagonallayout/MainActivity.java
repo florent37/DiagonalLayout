9db60764b6459c4ad33bbe297ed288e5c8998e7a
florent37 <champigny.florent@gmail.com>
2016-10-16 18:12:43 +0200

    uploaded v1

--- app/src/main/java/com/github/florent37/sample/diagonallayout/MainActivity.java
+++ app/src/main/java/com/github/florent37/sample/diagonallayout/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_simple);
+        setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
