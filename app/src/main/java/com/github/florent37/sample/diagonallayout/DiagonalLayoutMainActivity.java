6217a23ed19ea81a5487af5a1a4c98e5d1440c62
florent champigny <florent.champigny@backelite.com>
2017-09-27 18:08:33 +0200

    updated sample

--- app/src/main/java/com/github/florent37/sample/diagonallayout/DiagonalLayoutMainActivity.java
+++ app/src/main/java/com/github/florent37/sample/diagonallayout/DiagonalLayoutMainActivity.java
@@ -4,14 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class DiagonalLayoutMainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.diagonallayout_activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
